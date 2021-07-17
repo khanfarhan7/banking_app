d2afd5366fc5558693117d0ed694b6a47e941859
Farhan Khan <86169822+khanfarhan7@users.noreply.github.com>
2021-07-17 16:38:38 -0400

    new changes

--- atm.java
+++ atm.java
@@ -5,55 +5,82 @@ public class Atm {
 	public static void main(String[] args) {
 
 		System.out.println(
-				"\nPlease select an account type.\n\nPress 1 for Checking account, press 2 for Savings account or "
-						+ "press 3 to go back to the Main Menu");
+				"\nPlease select an option:"
+				+ "\n\n1.Checking account"
+				+ "\n2.Savings account"
+				+"\n3.Main Menu"
+				+"\n4.Check your account balance"
+				+"\n5.Exit");
 
 		Scanner sc = new Scanner(System.in);
 
-		int selection = sc.nextInt();
+		boolean session = true; // This variable will break the (while) loop when false
 
-		switch (selection) {
+		while (session) {
 
-		case 1:
+			int selection = sc.nextInt();
 
-			Methods met = new Methods();
+			switch (selection) {
 
-			met.checkingAcctMethod(selection);
+			case 1:
 
-			break;
+				Methods met = new Methods();
 
-		case 2:
+				met.checkingAcctMethod(selection);
 
-			Methods met1 = new Methods();
+				break;
 
-			met1.savingsAcctMethod(selection);
+			case 2:
 
-			break;
+				Methods met1 = new Methods();
 
-		case 3:
-			
-			Methods met2 = new Methods();
-			System.out.println(
-					"Main menu:\n\nPress 1 to select your checking account \nPress 2 to select your savings account");
-			int input3 = sc.nextInt();
-			if (input3 == 1) {
-				met2.checkingAcctMethod(input3);
+				met1.savingsAcctMethod(selection);
+
+				break;
+
+			case 3:
+
+				Methods met2 = new Methods();
+				System.out.println(
+						"Main menu:\n\nPress 1 to select your checking account \nPress 2 to select your savings account");
+				int input3 = sc.nextInt();
+				if (input3 == 1) {
+					met2.checkingAcctMethod(input3);
+				}
+
+				else if (input3 == 2) {
+					met2.savingsAcctMethod(input3);
+				}
+				break;
+				
+			case 4:
+				
+				System.out.println("Press 1 to check your checking account balance"
+						+ "or press 2 to check your savings account balance");
+				int input4 = sc.nextInt();
+				
+				if (input4==1) {
+					Account check1 = new Account ("checking", 0);
+					System.out.println("Your Checking account balanace is "+ check1.getBalance());
+				}
+				
+				if (input4==2) {
+					Account save1 = new Account ("savings", 5000);
+					System.out.println("Your Savings account balanace is "+ save1.getBalance());
+				}
+
+				break;	
+
+			case 5:
+				session = false;
+
+				break;
+
 			}
-
-			else if (input3 == 2) {
-				met2.savingsAcctMethod(input3);
-			}
-			break;
-
-		case 4:
-
-			System.out.println("Invalid option selected");
-			break;
-
 		}
+		System.out.println("Program Ended");
 		
 		sc.close();
-
 	}
 
 }
